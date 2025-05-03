33d8409ffdc90395324cc91a904df76e230af5d9
Gk1911krishna <148239306+Gk1911krishna@users.noreply.github.com>
2025-05-03 20:23:41 +0530

    Delete survey_distribution_app directory

--- survey_distribution_app/surveys/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('create/', views.create_survey, name='create_survey'),
-    path('city/<str:city>/', views.get_surveys_for_city, name='get_surveys_for_city'),
-    path('submit_response/', views.submit_survey_response, name='submit_survey_response'),
-]
